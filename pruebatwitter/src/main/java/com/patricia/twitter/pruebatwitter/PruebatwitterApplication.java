4210824bec7ed12cd3617c0e7fe5e89eb1935f7b
Patricia Tebar <7901494@alumnos.ufv.es>
2023-01-18 21:11:54 +0100

    he metido el front y el back

---
+++ pruebatwitter/src/main/java/com/patricia/twitter/pruebatwitter/PruebatwitterApplication.java
@@ -0,0 +1,13 @@
+package com.patricia.twitter.pruebatwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebatwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebatwitterApplication.class, args);
+	}
+
+}
---
+++ pruebatwitter/src/test/java/com/patricia/twitter/pruebatwitter/PruebatwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patricia.twitter.pruebatwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
